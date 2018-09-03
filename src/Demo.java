7e32ae0fb26af9d34ab459db51c284b3099f6c52
root <1215418388@qq.com>
2018-09-03 19:06:17 +0800

    增加了当时时间。

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("上传到GitHub的第一个案例");
+        Date date = new Date();
+        System.out.println(date);
     }
 }
